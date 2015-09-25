ffd3326f56e1410609d302d2cbe930d434544fad
Johnny Rodriguez <johnnydrodriguez@gmail.com>
2015-09-24 22:26:02 -0400

    These are my week 5 warm up modules.

---
+++ task_02.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""My is my Task 2 File"""
+
+import task_01.peanut
+
+TIME = task_01.peanut.BUTTER
---
+++ task_03.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""My is Task 3 File."""
+
+from task_01.peanut import BUTTER
+
+JELLY = BUTTER
